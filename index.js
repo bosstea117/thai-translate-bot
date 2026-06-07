c0761f6a002db560c6f3a9d4a0eef69cd2a78cbc
bosstea117 <gotozoo22@gmail.com>
2026-06-07 20:38:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -174,15 +174,24 @@ event.source.groupId
   continue;
 }
  // ===== 禁止私聊 =====
+// ===== 禁止一般人私聊 =====
 if (event.source.type !== "group") {
 
-  await client.replyMessage(event.replyToken, {
-    type: "text",
-    text: "此機器人僅限已授權群組使用，請將機器人加入群組後輸入「申請授權」。"
-  });
+  const adminDoc = await db
+    .collection("adminUsers")
+    .doc(event.source.userId)
+    .get();
 
-  continue;
-}     
+  if (!adminDoc.exists) {
+
+    await client.replyMessage(event.replyToken, {
+      type: "text",
+      text: "此機器人僅限已授權群組使用，請將機器人加入群組後輸入「申請授權」。"
+    });
+
+    continue;
+  }
+}
 // ===== 群組授權檢查 =====
 if (event.source.type === "group") {
 
